1928f2610c9190dd0916bb2cdff0482bfe0c8772
Flocky05 <md.rakibul.islam.0501@gmail.com>
2023-07-09 21:58:41 +0600

    fetching data from server

--- Js/index.js
+++ Js/index.js
@@ -1,1 +1,8 @@
-console.log("what the hell bro ?");
+const phoneLoad = async () => {
+  const url = "https://openapi.programming-hero.com/api/phones?search=iphone";
+  const res = await fetch(url);
+  const data = await res.json();
+  console.log(data.data);
+};
+
+phoneLoad();
